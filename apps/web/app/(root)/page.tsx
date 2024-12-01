8451fa63cd0ca82e19ad634287cce39b3ee5826b
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2024-12-01 18:23:51 +0530

    feat: added feature-section

--- apps/web/app/(root)/page.tsx
+++ apps/web/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/ui/Hero";
 import ProductDemo from "@/components/ui/ProductDemo";
+import FeatureSection from "@/components/ui/info-section";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <ProductDemo />
+      <FeatureSection/>
     </main>
   );
 }
--- apps/web/lib/constant.ts
+++ apps/web/lib/constant.ts
@@ -3,3 +3,21 @@ export const HOMEPAGE = {
     HERO_SECTION_DESCRIPTION: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti dignissimos incidunt libero sed sit eius, aut, culpa ex inventore unde ut est qui saepe consequuntur provident amet cum nostrum eos?',
     HERO_SECTION_BUTTON: 'Start Meeting'
 }
+export const FEATURE_SECTION = {
+    FEATURE_SECTION_HEADING: 'Features',
+    FEATURE_SECTION_SHORTDESC: 'Build whatever you want,&nbsp;seriously.',
+    FEATURE_SECTION_LONGDESC: 'Because Tailwind is so low-level, it never encourages you to design the same site twice. Even with the same color palette and sizing scale, its easy to build the same component with a completely different look in the next project.',
+    FEATURES:[{
+        TITLE: 'Feature 1',
+        IMAGE: 'https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75'
+    },{
+        TITLE: 'Feature 2',
+        IMAGE: 'https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75'
+    },{
+        TITLE: 'Feature 3',
+        IMAGE: 'https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75'
+    },{
+        TITLE: 'Feature 4',
+        IMAGE: 'https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75'
+    },]
+}
