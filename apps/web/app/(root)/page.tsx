f3c351d8d286c0fc906fa0ef83102d45e5ad860b
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2024-12-01 17:32:46 +0530

    product demo section

--- apps/web/app/(root)/page.tsx
+++ apps/web/app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/ui/Hero";
+import ProductDemo from "@/components/ui/ProductDemo";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ProductDemo />
     </main>
   );
 }
--- apps/web/components/ui/Hero.tsx
+++ apps/web/components/ui/Hero.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Hero = () => {
   return (
     <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
@@ -14,7 +12,7 @@ const Hero = () => {
       <div className="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
         <a
           href=""
-          className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+          className="bg-slate-900 hover:bg-slate-700 focus:outline-none text-white font-semibold h-12 px-6 rounded-lg flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
         >
           Start Meeting
         </a>
